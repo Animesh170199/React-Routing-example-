ea4da31a40cbdbed7117e21725e1ea790f26429d
Animesh170199 <animesh@strivemindz.com>
2022-08-15 16:44:43 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useState } from "react";
 import Accordion from "./Components/Accordion";
+import Dropdown from "./Components/Dropdown";
 import Search from "./Components/Search";
+import Translate from "./Components/Translate";
 const items = [
   {
     title: "what is React?",
@@ -15,11 +18,39 @@ const items = [
     content: "React is used by creating components",
   },
 ];
+const options = [
+  {
+    label: "The Color Orange",
+    value: "Orange",
+  },
+  {
+    label: "The Color Green",
+    value: "Green",
+  },
+  {
+    label: "The Color White",
+    value: "White",
+  },
+];
 function App() {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropDown, setShowDropDown] = useState(true);
+
   return (
     <div>
       <Accordion items={items} />
       <Search />
+      <button onClick={() => setShowDropDown(!showDropDown)}>
+        Toggle DropDown
+      </button>
+      {showDropDown ? (
+        <Dropdown
+          onSelectedChange={setSelected}
+          selected={selected}
+          options={options}
+        />
+      ) : null}
+      <Translate />
     </div>
   );
 }
